5cfd5d0174dd2ded3de253a9b2fc4b013729bbc4
mehmetemin <kacmazemin72@gmail.com>
2020-10-05 23:34:52 +0300

    feat: created GameMode cpp

---
+++ Source/Stroll/StrollGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StrollGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STROLL_API AStrollGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
