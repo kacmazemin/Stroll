86f742b6c848c70511d9fd8eedafb0020d140728
mehmetemin <kacmazemin72@gmail.com>
2020-10-05 23:34:41 +0300

    feat: init project

---
+++ Source/Stroll/Stroll.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Stroll.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Stroll, "Stroll" );
