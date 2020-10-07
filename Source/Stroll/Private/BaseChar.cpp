0a1fd4f102e68082d55b384ded5351a2df2ecc71
mehmetemin <kacmazemin72@gmail.com>
2020-10-07 22:09:20 +0300

    feat: change camera control

--- Source/Stroll/Private/BaseChar.cpp
+++ Source/Stroll/Private/BaseChar.cpp
@@ -3,6 +3,8 @@
 
 #include "BaseChar.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ABaseChar::ABaseChar()
@@ -10,13 +12,26 @@ ABaseChar::ABaseChar()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
-	CameraComponent->SetupAttachment(RootComponent);
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 
-	// Position the camera slightly above the eyes.
-	CameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 100.0f + BaseEyeHeight));
-	// Allow the pawn to control camera rotation.
-	CameraComponent->bUsePawnControlRotation = true;
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 600.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->TargetArmLength = 350.0f; // The camera follows at this distance behind the character	
+	SpringArmComponent->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+		
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->AttachTo(SpringArmComponent, USpringArmComponent::SocketName);
+	CameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
 }
 
 // Called when the game starts or when spawned
@@ -30,6 +45,9 @@ void ABaseChar::BeginPlay()
 void ABaseChar::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	FRotator newRotator = GetActorRotation();
+	FRotator newPitch = SpringArmComponent->GetComponentRotation();
 
 }
 
--- Source/Stroll/Public/BaseChar.h
+++ Source/Stroll/Public/BaseChar.h
@@ -19,6 +19,9 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere)
+	class USpringArmComponent* SpringArmComponent;
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
