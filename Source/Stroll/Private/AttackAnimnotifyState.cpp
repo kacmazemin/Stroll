11f61c3c33d104f1badc1d59056816a58d87c5cf
mehmetemin <kacmazemin72@gmail.com>
2020-10-25 21:48:01 +0300

    feat: implemented NotifyTick

--- Source/Stroll/Private/AttackAnimnotifyState.cpp
+++ Source/Stroll/Private/AttackAnimnotifyState.cpp
@@ -5,9 +5,27 @@
 
 #include "DrawDebugHelpers.h"
 
-void UAttackAnimnotifyState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation,
-                                        float FrameDeltaTime)
+void UAttackAnimnotifyState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime)
 {
-
+    if(MeshComp)
+    {
+        /*/////*/
+        FHitResult Result;
+        FVector StartPos = MeshComp->GetSocketLocation("SocketTrail_End");
+        FVector EndPos = MeshComp->GetSocketLocation("SocketTrail_Start");
+    
+        FCollisionQueryParams Params;
+        Params.bTraceComplex = true;
+        
+        DrawDebugCapsule(MeshComp->GetWorld(), EndPos, 50.f, 15.f, MeshComp->GetSocketQuaternion("SocketTrail_End"), FColor::Black, false,5.f);
+        
+        bool bHit = MeshComp->GetWorld()->SweepSingleByChannel(Result, StartPos, EndPos, FQuat::Identity, ECC_Pawn, FCollisionShape::MakeCapsule(15.f,50.f), Params);
+    
+        if(bHit && Result.GetActor())
+        {
+            DrawDebugCapsule(MeshComp->GetWorld(), Result.ImpactPoint, 50.f, 15.f, MeshComp->GetSocketQuaternion("SocketTrail_End"), FColor::Purple, false,5.f);
+        }
+    
+    }
         
 }
