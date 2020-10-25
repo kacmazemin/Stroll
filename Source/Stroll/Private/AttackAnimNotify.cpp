ff01c8610e678ecfa2d67d3bc26426775b1985c9
mehmetemin <kacmazemin72@gmail.com>
2020-10-25 21:46:10 +0300

    feat: created AttackAnimNotify class

---
+++ Source/Stroll/Private/AttackAnimNotify.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttackAnimNotify.h"
+
+#include "DrawDebugHelpers.h"
+
+
+void UAttackAnimNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation)
+{
+   // GEngine->AddOnScreenDebugMessage(-1, 4.f, FColor::Black, TEXT("ATTACK NOTIFY"));
+}
