85c6f10e32ed1ecc5bd29358105ae1975724c5ec
mehmetemin <kacmazemin72@gmail.com>
2020-10-28 23:53:11 +0300

    feat: created CameraShake Class

---
+++ Source/Stroll/Public/BaseCharCameraShake.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraShake.h"
+#include "BaseCharCameraShake.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STROLL_API UBaseCharCameraShake : public UCameraShake
+{
+	GENERATED_BODY()
+	
+};
